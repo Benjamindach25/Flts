ffc7e9ab09f7c22276205e75f9460f7f004efee7
Mayavan <149401503+Benjamindach25@users.noreply.github.com>
2024-09-20 13:13:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,6 +21,10 @@ import pytz
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from tobot import tobot
+from pyrogram import utils as pyroutils
+
+pyroutils.MIN_CHAT_ID = -999999999999
+pyroutils.MIN_CHANNEL_ID = -100999999999999
 
 class Bot(Client):
 
